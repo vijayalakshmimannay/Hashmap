6441c20562650a861450a04a4b4fa59b2d846b52
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-20 18:09:11 +0530

    [vijayalakshmi] frequency of words in hash map

---
+++ Hashmap/FrequencyOfWords.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashMapprogram
+{
+    internal class FrequencyOfWords<K, V>
+    {
+        LinkedList<KeyValue<K, V>> list = new LinkedList<KeyValue<K, V>>();
+        public void add(K key, V value)
+        {
+            KeyValue<K, V> kv = new KeyValue<K, V>() { key = key, value = value };
+            list.AddLast(kv);
+        }
+        public void getFrequencyOfWords()
+        {
+            int count = 0;
+            if (list == null)
+            {
+                Console.WriteLine("Hash Table is empty.");
+                return;
+            }
+            foreach (KeyValue<K, V> item in list)
+            {
+                foreach (KeyValue<K, V> value in list)
+                {
+                    if (item.value.Equals(value.value))
+                        count++;
+                }
+                Console.WriteLine("Count of " + item.value + " is " + count);
+                count = 0;
+            }
+        }
+    }
+    public struct KeyValue<K, V>
+    {
+        public K key;
+        public V value;
+    }
+}
---
+++ Hashmap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HashMapprogram;
+
+namespace HashAndTrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table Program Started");
+            FrequencyOfWords<string, string> hash = new FrequencyOfWords<string, string>();
+            Console.WriteLine("Frequency of words is:");
+            hash.add("0", "vijayalakshmi");
+            hash.add("1", "harsha");
+            hash.add("2", "aarna");
+            hash.add("3", "harsha");
+            hash.add("4", "sony");
+            hash.add("5", "sita");
+            hash.getFrequencyOfWords();
+
+
+        }
+    }
+}
