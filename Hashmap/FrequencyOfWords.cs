ac2584292bd1fb350851fdf1e85c59cc7e73ddd0
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-20 19:45:20 +0530

    [vijayalakshmi] word frequency

--- Hashmap/FrequencyOfWords.cs
+++ Hashmap/FrequencyOfWords.cs
@@ -8,30 +8,101 @@ namespace HashMapprogram
 {
     internal class FrequencyOfWords<K, V>
     {
-        LinkedList<KeyValue<K, V>> list = new LinkedList<KeyValue<K, V>>();
-        public void add(K key, V value)
+
+        int size;
+        LinkedList<KeyValue<K, V>>[] list;
+        public FrequencyOfWords(int size)
         {
-            KeyValue<K, V> kv = new KeyValue<K, V>() { key = key, value = value };
-            list.AddLast(kv);
+            this.size = size;
+            this.list = new LinkedList<KeyValue<K, V>>[size];
         }
-        public void getFrequencyOfWords()
+
+        public void Add(K key, V value)
+        {
+            int index = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> newList = getLinkedList(index);
+            KeyValue<K, V> kv = new KeyValue<K, V>() { key = key, value = value };
+            newList.AddLast(kv);
+        }
+        public int getFrequency(string value)
         {
             int count = 0;
             if (list == null)
             {
                 Console.WriteLine("Hash Table is empty.");
-                return;
+                return 0;
             }
-            foreach (KeyValue<K, V> item in list)
+            for (int i = 0; i < list.Length; i++)
             {
-                foreach (KeyValue<K, V> value in list)
+                LinkedList<KeyValue<K, V>> newList = getLinkedList(i);
+                foreach (KeyValue<K, V> items in newList)
                 {
-                    if (item.value.Equals(value.value))
+                    if (items.value.Equals(value))
                         count++;
                 }
-                Console.WriteLine("Count of " + item.value + " is " + count);
-                count = 0;
             }
+            Console.WriteLine("Count of " + value + " is " + count);
+            return count;
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+        public int GetArrayPosition(K key)
+        {
+            int index = key.GetHashCode() % size;
+            return Math.Abs(index);
+        }
+        public LinkedList<KeyValue<K, V>> getLinkedList(int index)
+        {
+            LinkedList<KeyValue<K, V>> newList = list[index];
+            if (newList == null)
+            {
+                newList = new LinkedList<KeyValue<K, V>>();
+                list[index] = newList;
+            }
+            return newList;
+        }
+        public bool ContainsKey(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.key.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public V GetValue(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.key.Equals(key))
+                {
+                    return item.value;
+                }
+            }
+            return default(V);
         }
     }
     public struct KeyValue<K, V>
@@ -40,3 +111,5 @@ namespace HashMapprogram
         public V value;
     }
 }
+
+
--- Hashmap/Program.cs
+++ Hashmap/Program.cs
@@ -7,7 +7,7 @@ namespace HashAndTrees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hash Table Program Started");
+            /* Console.WriteLine("Hash Table Program Started");
             FrequencyOfWords<string, string> hash = new FrequencyOfWords<string, string>();
             Console.WriteLine("Frequency of words is:");
             hash.add("0", "vijayalakshmi");
@@ -16,9 +16,38 @@ namespace HashAndTrees
             hash.add("3", "harsha");
             hash.add("4", "sony");
             hash.add("5", "sita");
-            hash.getFrequencyOfWords();
+            hash.getFrequencyOfWords();*/
+            string phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] words = phrase.ToLower().Split(" ");
+            var dist = words.Distinct();
+            int length = 0;
+            foreach (var word in dist)
+                length++;
+            HashMapprogram.FrequencyOfWords<string, int> map = new HashMapprogram.FrequencyOfWords<string, int>(length);
+            int count = 1;
+            foreach (string word in words)
+            {
+                if (map.ContainsKey(word))
+                {
+                    count = map.GetValue(word) + 1;
+                    map.Remove(word);
+                    map.Add(word, count);
+                }
+                else
+                {
+                    map.Add(word, 1);
+                }
+            }
+            Console.WriteLine($"{"Frequency"} | {"Count"}\n");
+            foreach (string key in dist)
+            {
+                Console.WriteLine($"{key} | {map.GetValue(key)}");
+            }
 
 
         }
+
+
     }
+    
 }
